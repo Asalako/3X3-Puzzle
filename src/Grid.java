97a5925843bd01fc89cd4bc22d75e6b1f7c61d8f
Asalako <au_salako@hotmail.com>
2019-12-01 21:17:35 +0000

    Added Maximum depth level

--- src/Grid.java
+++ src/Grid.java
@@ -4,16 +4,16 @@ public class Grid {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		State state1 = new State("abcdefgh_");
+		State state2 = new State("abcdefg_h");
 		//System.out.println(state1.getStateGrid()[0]);
 //		ArrayList<State> states = state1.nextState();
 //		for (State state : states) {
 //			state.outputGrid();
 //			System.out.println("");
 //		}
-		state1.outputGrid();
-		state1.combinations(state1);
-		state1.sizeOfTraversedStates();
-
+		state1.combinations(state1, 10);
+		state2.combinations(state2, 10);
+		State.compare(state1, state2);
 	}
 
 }
