145a131a8e54a0d411270c7206b9908587785fa8
Asalako <au_salako@hotmail.com>
2019-12-10 01:12:42 +0000

    Added feature to write states to a file

--- src/Grid.java
+++ src/Grid.java
@@ -1,22 +1,45 @@
+import java.util.Scanner;
+import java.io.IOException;
+import java.util.HashSet;
 
 public class Grid {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		State state1 = new State("abcdefgh_");
-		State state2 = new State("abcdefg_h");
-		//System.out.println(state1.getStateGrid()[0]);
-//		ArrayList<State> states = state1.nextState();
-//		for (State state : states) {
-//			state.outputGrid();
-//			System.out.println("");
-//		}
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the first State: ");
+		State state1 = new State(scanner.next());
 		state1.combinations(state1);
-//		state2.combinations(state2);
-		State.compare(state1, state2);
-		state1.outputTree(10);
 		
-//		state2.outputTree();
+		System.out.print(
+				"Would you like write all possible states to a file? Enter y/Y for yes any other character for no. ");
+		
+		if (scanner.next().toLowerCase().equals("y")) {
+			state1.outputTree(10, "state1.txt");
+		}
+	
+		
+		System.out.print("Enter the second State: ");
+		State state2 = new State(scanner.next());
+		state2.combinations(state2);
+	
+		System.out.print(
+				"Would you like write all possible states to a file? Enter y/Y for yes any other character for no. ");
+
+		if (scanner.next().toLowerCase().equals("y")) {
+			state2.outputTree(10, "state2.txt");
+		}
+		
+		HashSet<String> diffStates = State.compare(state1, state2);
+				
+		for (String state: diffStates) {
+			new State(state).outputGrid();
+		}
+		
+		System.out.println("\nNumber of states in State 1: " + state1.getTraversedStates().size());
+		System.out.println("Number of states in State 2: " + state2.getTraversedStates().size());
+		System.out.println("Number of states in State 1 but not in State 2 is: " + diffStates.size() );
+
 	}
 
 }
--- src/State.java
+++ src/State.java
@@ -1,4 +1,10 @@
-import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class State {
 	
@@ -20,11 +26,11 @@ public class State {
 	 * Finds all possibles distinct states that can be achieved through x moves recursively
 	 * 
 	 * @param	currentState - takes in a state to be computed.
-	 * @returns	
+	 * @returns	returns true to know if last node of the tree has been reach 
 	 * 
 	 */
 	public boolean combinations(State currentState) {
-		//The current state is checked against every state that's been visited, returns if it already exists
+		//The current state is checked against every state that's been visited, returns true if it already exists
 		boolean backTrack = false;
 		
 		if (traversedStates.contains(currentState.getStateString())) {
@@ -40,12 +46,15 @@ public class State {
 		currentState.outputGrid();
 		System.out.println();
 		
-		//Next possible states are retrieved, then checked if it exists recursively
+		//Next possible states are retrieved based on the current state
 		ArrayList<State> states = nextState(currentState);
 		
+		//Next possible states are checked to see if it exists recursively
 		for (State nextState : states) {
+			//If last node of the tree has been reach. It will backtrack to find the next working tree and output
+			//That node, to know where it went back to
 			if (!traversedStates.contains(nextState.getStateString()) && backTrack == true) {
-				System.out.println("back to");
+				System.out.println("End of tree. backtracking to");
 				currentState.outputGrid();
 			}
 			backTrack = combinations(nextState);
@@ -54,6 +63,12 @@ public class State {
 		return true;
 	}
 	
+	/*
+	 * Gets the valid moves that can be made from the given state
+	 * 
+	 * @param	currentState - takes in a state to find the next possible moves
+	 * @returns array of states
+	 */
 	public ArrayList<State> nextState(State currentState) {
 		int[] position = currentState.getPosition();
 		ArrayList<State> states = new ArrayList<State>();
@@ -61,7 +76,7 @@ public class State {
 		char[][] stateCharArray;
 		char charswap;
 		
-		//Checks if an element is above the current position, to then make a swap. A new state is created and stored
+		//Based on the x,y position of the empty space, checks if it can swap with an element above
 		if (position[0] > 0 && position[0] <=2) {
 			stateCopy = copy( currentState.getState() );
 			stateCharArray = toListOfChars(stateCopy);
@@ -72,7 +87,7 @@ public class State {
 			states.add(new State( toStateString(stateCopy) ));
 		}
 		
-		//Checks if an element is below...
+		//Checks if a swap can be made below
 		if (position[0] >= 0 && position[0] <2) {
 			stateCopy = copy( currentState.getState() );
 			stateCharArray = toListOfChars(stateCopy);
@@ -83,7 +98,7 @@ public class State {
 			states.add(new State( toStateString(stateCopy) ));
 		}
 		
-		//Checks if an element is to the left
+		//Checks if a swap can be made to the left
 		if (position[1] > 0 && position[1] <=2) {
 			stateCopy = copy( currentState.getState() );
 			stateCharArray = toListOfChars(stateCopy);
@@ -94,7 +109,7 @@ public class State {
 			states.add(new State( toStateString(stateCopy) ));
 		}
 		
-		//Checks if an element is to the right
+		//Checks if a swap can be made to the right
 		if (position[1] >= 0 && position[1] <2) {
 			stateCopy = copy( currentState.getState() );
 			stateCharArray = toListOfChars(stateCopy);
@@ -109,9 +124,16 @@ public class State {
 		
 	}
 	
+	/*
+	 * Gets the index position of the _ (empty space in the state) in the 2d array
+	 * 
+	 * @returns array of ints
+	 */
 	public int[] getPosition() {
 		int[] position = new int[2];
 		int index;
+		
+		//For each row in the 2d array, checks if it contains the _
 		for (int i = 0; i < 3; i++) {
 			index = this.getState()[i].indexOf("_");
 			if (index > -1 ) {
@@ -123,11 +145,23 @@ public class State {
 		return position;
 	}
 	
+	/*
+	 * Creates a copy of a 2d array
+	 * 
+	 * @param	state - the state to copy
+	 * @returns	2d array
+	 */
 	static public String[] copy(String[] state) {
 		String[] stateCopy = { state[0], state[1], state[2] };
 		return stateCopy;
 	}
 	
+	/*
+	 * Converts each character in the string array into an array of a single character
+	 * 
+	 * @param	state - state to convert
+	 * @returns	2d array of characters
+	 */
 	static public char[][] toListOfChars(String[] state) {
 		char[][] charArray = new char[3][3];
 		for (int i = 0; i < 3; i++) {
@@ -137,11 +171,20 @@ public class State {
 
 	}
 	
+	/*
+	 * Converts 2d array of characters into array of strings
+	 * 
+	 * @param	charArray - 2d array of characters to convert
+	 * @returns	array of strings
+	 */
 	static public String[] charListToString(char[][] charArray) {
 		String[] state = { new String(charArray[0]), new String(charArray[1]), new String(charArray[2]) };
 		return state;
 	}
 	
+	/*
+	 * Converts 
+	 */
 	static public String toStateString(String[] state) {
 		return state[0] + state[1] + state[2];
 	}
@@ -172,72 +215,76 @@ public class State {
 		char[][] charArray = toListOfChars(state);
 		System.out.println("| " + charArray[0][0] + " |" + "| " + charArray[0][1] + " |" + "| " + charArray[0][2] + " |");
 		System.out.println("| " + charArray[1][0] + " |" + "| " + charArray[1][1] + " |" + "| " + charArray[1][2] + " |");
-		System.out.println("| " + charArray[2][0] + " |" + "| " + charArray[2][1] + " |" + "| " + charArray[2][2] + " |");
+		System.out.println("| " + charArray[2][0] + " |" + "| " + charArray[2][1] + " |" + "| " + charArray[2][2] + " |\n");
 
 	}
 	
 	static public String[] stringToState(String str) {
-		String[] state = new String[3];
-		state[0] = str.substring(0, 3);
-		state[1] = str.substring(3, 6);
-		state[2] = str.substring(6, 9);
+		String[] state = { str.substring(0, 3), str.substring(3, 6), str.substring(6, 9) };
 		return state;
 	}
 	
-	public void outputTree(int statesPerLine) {
+	public void outputTree(int statesPerLine, String fileName) throws IOException {
 		ArrayList<State> copyOfOutputTree = new ArrayList<State>(outputTree);
 		ArrayList<State> copyOfStates = new ArrayList<State>();
 		
 		int noOfLines = Math.floorDiv(traversedStates.size(), statesPerLine);
-		System.out.println(noOfLines);
+		FileWriter file = new FileWriter(fileName);
 		int i = 0;
 		int j = 1;
+		String str;
 		
 		while (i < noOfLines) {
+			str = "";
 			for (int k = 0; k < statesPerLine; k++) {
 				State state =  copyOfOutputTree.get(k);
 				char[][] charArray = toListOfChars(state.getState());
 				copyOfStates.add(state);
 				
 				if (j < statesPerLine - 1) {
-					System.out.print("| " + charArray[0][0] + " |" + "| " + charArray[0][1] + " |" + "| " + charArray[0][2] + " |      ");
+					str += "| " + charArray[0][0] + " |" + "| " + charArray[0][1] + " |" + "| " + charArray[0][2] + " |      ";
 					j++;
 				} 
 				else {
-					System.out.print("| " + charArray[0][0] + " |" + "| " + charArray[0][1] + " |" + "| " + charArray[0][2] + " |      ");
+					str +="| " + charArray[0][0] + " |" + "| " + charArray[0][1] + " |" + "| " + charArray[0][2] + " |      ";
 					j = 0;
 				}
 			}
-			System.out.println();
-
+			str += "\n";
+			file.write(str);
+			str = "";
+			
 			for (State stateCopy: copyOfStates) {
 				char[][] grid = toListOfChars(stateCopy.getState());
 
 				if (j < statesPerLine - 1) {
-					System.out.print("| " + grid[1][0] + " |" + "| " + grid[1][1] + " |" + "| " + grid[1][2] + " |      ");
+					str += "| " + grid[1][0] + " |" + "| " + grid[1][1] + " |" + "| " + grid[1][2] + " |      ";
 					j++;
 				} 
 				else {
-					System.out.print("| " + grid[1][0] + " |" + "| " + grid[1][1] + " |" + "| " + grid[1][2] + " |      ");
+					str += "| " + grid[1][0] + " |" + "| " + grid[1][1] + " |" + "| " + grid[1][2] + " |      ";
 					j = 0;
 				}
 			}
-			System.out.println();
+			str += "\n";
+			file.write(str);
+			str = "";
 
 			for (State stateCopy: copyOfStates) {
 				char[][] grid = toListOfChars(stateCopy.getState());
 				if (j < statesPerLine - 1) {
-					System.out.print("| " + grid[2][0] + " |" + "| " + grid[2][1] + " |" + "| " + grid[2][2] + " |      ");
+					str += "| " + grid[2][0] + " |" + "| " + grid[2][1] + " |" + "| " + grid[2][2] + " |      ";
 					j++;
 				} 
 				else {
-					System.out.print("| " + grid[2][0] + " |" + "| " + grid[2][1] + " |" + "| " + grid[2][2] + " |      ");
+					str += "| " + grid[2][0] + " |" + "| " + grid[2][1] + " |" + "| " + grid[2][2] + " |      ";
 					j = 0;
 				}
 			}
 			
-			System.out.println();
-			System.out.println();
+			str += "\n\n";
+			file.write(str);
+			str = "";
 				
 			for (State stateCopy: copyOfStates) {
 				copyOfOutputTree.remove(stateCopy);
@@ -245,7 +292,28 @@ public class State {
 			copyOfStates.clear();
 			i++;
 		}
+		file.close();
 		
+	}
+	
+	static public String stateValidation(String input) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while ( input.length() != 9 || !input.contains("_") ) {
+			if (input.length() != 9) {
+				System.out.println("The state must have 9 characters.");
+				System.out.print("Enter a valid State: ");
+				input = scanner.next();
+			}
+			else if ( !input.contains("_") ) {
+				System.out.println("The state must contain an underscore. _");
+				System.out.print("Enter a valid State: ");
+				input = scanner.next();
+			}
+			
+		}
+		
+		return input;
 	}
 	
 	static public HashSet<String> compare(State state1, State state2) {
@@ -258,7 +326,7 @@ public class State {
 				distinctStates.add(currentState);
 			}
 		}
-		System.out.println(distinctStates.size());
+		
 		return distinctStates;
 	}
 	
